147a37a1f29163166f435fc46fdb62d25959c18b
Aurel Rothärmel <aurel.rothaermel@gmx.de>
2023-12-09 09:12:15 +0100

    Reformat 2023 Day 9

--- rust/aoc2023/src/bin/day-09.rs
+++ rust/aoc2023/src/bin/day-09.rs
@@ -43,15 +43,9 @@ fn part2(input: &str) -> String {
         .to_string()
 }
 
-#[allow(unused_imports)]
 fn parse_input(input: &str) -> nom::IResult<&str, Vec<Vec<NumTy>>> {
-    use nom::branch::alt;
-    use nom::bytes::complete::tag;
-    use nom::character::complete::{alpha1, alphanumeric1, newline, space1};
-    use nom::combinator::map;
-    use nom::combinator::value;
-    use nom::multi::{many1, separated_list1};
-    use nom::sequence::{delimited, preceded, separated_pair, terminated};
+    use nom::character::complete::{newline, space1};
+    use nom::multi::separated_list1;
 
     separated_list1(newline, separated_list1(space1, num_parser))(input)
 }
@@ -62,7 +56,8 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        let input = "0 3 6 9 12 15
+        let input = "\
+0 3 6 9 12 15
 1 3 6 10 15 21
 10 13 16 21 30 45
 ";
@@ -72,7 +67,8 @@ mod tests {
 
     #[test]
     fn test_part2() {
-        let input = "0 3 6 9 12 15
+        let input = "\
+0 3 6 9 12 15
 1 3 6 10 15 21
 10 13 16 21 30 45
 ";
