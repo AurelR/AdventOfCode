6c9ee60b6aefbd5b18726311f61988a9b8e62887
Aurel Rothärmel <aurel.rothaermel@gmx.de>
2023-12-14 11:30:20 +0100

    Solve 2023 Day 14 Part 2

--- rust/aoc2023/src/bin/day-14.rs
+++ rust/aoc2023/src/bin/day-14.rs
@@ -1,6 +1,6 @@
 type NumTy = i32;
 type NumPair = (NumTy, NumTy);
-use std::collections::BTreeMap;
+use std::collections::{BTreeMap, HashMap};
 
 fn main() {
     let input = std::fs::read_to_string("data/input/input14.txt").unwrap();
@@ -13,39 +13,34 @@ fn main() {
 fn part1(input: &str) -> String {
     let data = parse_input(input).unwrap().1;
     let grid = reformat_data(data);
-    let mut new_grid = grid.clone();
-    let mut load = 0;
-    for x in 0..grid.x_size {
-        for y in 0..grid.y_size {
-            match new_grid.contents.get(&(x, y)) {
-                Some(Location::SquareRock) => continue,
-                Some(Location::RoundedRock) => {
-                    load += grid.y_size - y;
-                    continue;
-                }
-                Some(Location::Empty) => {}
-                None => unreachable!("Outside grid"),
-            }
-
-            if let Some(test_y) = (y + 1..grid.y_size)
-                .find(|fy| *new_grid.contents.get(&(x, *fy)).unwrap() != Location::Empty)
-            {
-                let test_pos = new_grid.contents.get_mut(&(x, test_y)).unwrap();
-                if *test_pos == Location::RoundedRock {
-                    *test_pos = Location::Empty;
-                    load += grid.y_size - y;
-                    *new_grid.contents.get_mut(&(x, y)).unwrap() = Location::RoundedRock;
-                }
-            }
-        }
-    }
-    dbg!(new_grid);
-    load.to_string()
+    grid.tilt_north().load().to_string()
 }
 
 fn part2(input: &str) -> String {
-    let _data = parse_input(input).unwrap().1;
-    "".to_string()
+    let data = parse_input(input).unwrap().1;
+    let mut grid = reformat_data(data);
+    let mut cache = HashMap::new();
+    let mut n0 = 0;
+    let mut n1 = 0;
+    for c in 0.. {
+        let new_grid = grid.cycle();
+        cache.insert(grid, c);
+        if let Some(c0) = cache.get(&new_grid) {
+            n0 = *c0;
+            n1 = c;
+            break;
+        }
+        grid = new_grid;
+    }
+
+    let a = 1_000_000_000 - n0;
+    let b = n1 - n0 + 1;
+    let c = a % b;
+    let n = c + n0;
+    let result = cache
+        .iter()
+        .find_map(|(k, v)| if *v == n { Some(k) } else { None });
+    result.unwrap().load().to_string()
 }
 
 #[allow(unused_imports)]
@@ -86,18 +81,143 @@ fn reformat_data(data: Vec<Vec<Location>>) -> Grid {
     }
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 enum Location {
     Empty,
     SquareRock,
     RoundedRock,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
 struct Grid {
     contents: BTreeMap<NumPair, Location>,
     x_size: NumTy,
     y_size: NumTy,
+}
+
+impl Grid {
+    fn cycle(&self) -> Grid {
+        self.tilt_north().tilt_west().tilt_south().tilt_east()
+    }
+    fn load(&self) -> NumTy {
+        let mut load = 0;
+        for x in 0..self.x_size {
+            for y in 0..self.y_size {
+                match self.contents.get(&(x, y)) {
+                    Some(Location::SquareRock) => continue,
+                    Some(Location::RoundedRock) => {
+                        load += self.y_size - y;
+                        continue;
+                    }
+                    Some(Location::Empty) => {}
+                    None => unreachable!("Outside grid"),
+                }
+            }
+        }
+        load
+    }
+
+    fn tilt_north(&self) -> Grid {
+        let mut tilted_grid = self.clone();
+        for x in 0..self.x_size {
+            for y in 0..self.y_size {
+                match tilted_grid.contents.get(&(x, y)) {
+                    Some(Location::Empty) => {}
+                    Some(Location::SquareRock) => continue,
+                    Some(Location::RoundedRock) => continue,
+                    None => unreachable!("Outside grid"),
+                }
+
+                if let Some(test_y) = (y + 1..self.y_size)
+                    .find(|fy| *tilted_grid.contents.get(&(x, *fy)).unwrap() != Location::Empty)
+                {
+                    let test_pos = tilted_grid.contents.get_mut(&(x, test_y)).unwrap();
+                    if *test_pos == Location::RoundedRock {
+                        *test_pos = Location::Empty;
+                        *tilted_grid.contents.get_mut(&(x, y)).unwrap() = Location::RoundedRock;
+                    }
+                }
+            }
+        }
+        tilted_grid
+    }
+
+    fn tilt_west(&self) -> Grid {
+        let mut tilted_grid = self.clone();
+        for y in 0..self.y_size {
+            for x in 0..self.x_size {
+                match tilted_grid.contents.get(&(x, y)) {
+                    Some(Location::Empty) => {}
+                    Some(Location::SquareRock) => continue,
+                    Some(Location::RoundedRock) => continue,
+                    None => unreachable!("Outside grid"),
+                }
+
+                if let Some(test_x) = (x + 1..self.x_size)
+                    .find(|fx| *tilted_grid.contents.get(&(*fx, y)).unwrap() != Location::Empty)
+                {
+                    let test_pos = tilted_grid.contents.get_mut(&(test_x, y)).unwrap();
+                    if *test_pos == Location::RoundedRock {
+                        *test_pos = Location::Empty;
+                        *tilted_grid.contents.get_mut(&(x, y)).unwrap() = Location::RoundedRock;
+                    }
+                }
+            }
+        }
+        tilted_grid
+    }
+
+    fn tilt_south(&self) -> Grid {
+        let mut tilted_grid = self.clone();
+        for x in 0..self.x_size {
+            for y in (0..self.y_size).rev() {
+                match tilted_grid.contents.get(&(x, y)) {
+                    Some(Location::Empty) => {}
+                    Some(Location::SquareRock) => continue,
+                    Some(Location::RoundedRock) => continue,
+                    None => unreachable!("Outside grid"),
+                }
+
+                if let Some(test_y) = (0..y)
+                    .rev()
+                    .find(|fy| *tilted_grid.contents.get(&(x, *fy)).unwrap() != Location::Empty)
+                {
+                    let test_pos = tilted_grid.contents.get_mut(&(x, test_y)).unwrap();
+                    if *test_pos == Location::RoundedRock {
+                        *test_pos = Location::Empty;
+                        *tilted_grid.contents.get_mut(&(x, y)).unwrap() = Location::RoundedRock;
+                    }
+                }
+            }
+        }
+        tilted_grid
+    }
+
+    fn tilt_east(&self) -> Grid {
+        let mut tilted_grid = self.clone();
+        for y in 0..self.y_size {
+            for x in (0..self.x_size).rev() {
+                match tilted_grid.contents.get(&(x, y)) {
+                    Some(Location::Empty) => {}
+                    Some(Location::SquareRock) => continue,
+                    Some(Location::RoundedRock) => continue,
+                    None => unreachable!("Outside grid"),
+                }
+
+                if let Some(test_x) = (0..x)
+                    .rev()
+                    .find(|fx| *tilted_grid.contents.get(&(*fx, y)).unwrap() != Location::Empty)
+                {
+                    let test_pos = tilted_grid.contents.get_mut(&(test_x, y)).unwrap();
+                    if *test_pos == Location::RoundedRock {
+                        *test_pos = Location::Empty;
+                        *tilted_grid.contents.get_mut(&(x, y)).unwrap() = Location::RoundedRock;
+                    }
+                }
+            }
+        }
+        tilted_grid
+    }
 }
 
 #[cfg(test)]
@@ -123,11 +243,115 @@ O.#..O.#.#
     }
 
     #[test]
-    #[ignore = "not ready yet"]
     fn test_part2() {
         let input = "\
-        ";
+O....#....
+O.OO#....#
+.....##...
+OO.#O....O
+.O.....O#.
+O.#..O.#.#
+..O..#O..O
+.......O..
+#....###..
+#OO..#....
+";
         let result = part2(input);
-        assert_eq!(result, "todo");
+        assert_eq!(result, "64");
+    }
+
+    #[test]
+    fn test_cycle1() {
+        let input = "\
+O....#....
+O.OO#....#
+.....##...
+OO.#O....O
+.O.....O#.
+O.#..O.#.#
+..O..#O..O
+.......O..
+#....###..
+#OO..#....
+";
+        let expected_input = "\
+.....#....
+....#...O#
+...OO##...
+.OO#......
+.....OOO#.
+.O#...O#.#
+....O#....
+......OOOO
+#...O###..
+#..OO#....
+";
+        let result = reformat_data(parse_input(input).unwrap().1).cycle();
+        let expected = reformat_data(parse_input(expected_input).unwrap().1);
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_cycle2() {
+        let input = "\
+.....#....
+....#...O#
+...OO##...
+.OO#......
+.....OOO#.
+.O#...O#.#
+....O#....
+......OOOO
+#...O###..
+#..OO#....
+";
+
+        let expected_input = "\
+.....#....
+....#...O#
+.....##...
+..O#......
+.....OOO#.
+.O#...O#.#
+....O#...O
+.......OOO
+#..OO###..
+#.OOO#...O
+";
+        let result = reformat_data(parse_input(input).unwrap().1).cycle();
+        let expected = reformat_data(parse_input(expected_input).unwrap().1);
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_cycle3() {
+        let input = "\
+.....#....
+....#...O#
+.....##...
+..O#......
+.....OOO#.
+.O#...O#.#
+....O#...O
+.......OOO
+#..OO###..
+#.OOO#...O
+";
+
+        let expected_input = "\
+.....#....
+....#...O#
+.....##...
+..O#......
+.....OOO#.
+.O#...O#.#
+....O#...O
+.......OOO
+#...O###.O
+#.OOO#...O
+";
+        let result = reformat_data(parse_input(input).unwrap().1).cycle();
+        let expected = reformat_data(parse_input(expected_input).unwrap().1);
+        assert_eq!(result, expected);
     }
 }
