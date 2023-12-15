f1807a550315622e5c0d50d44fc4ddc06488ecf2
Aurel Rothärmel <aurel.rothaermel@gmx.de>
2023-12-15 07:57:52 +0100

    Solve 2023 Day 15 Part 2

--- rust/aoc2023/src/bin/day-15.rs
+++ rust/aoc2023/src/bin/day-15.rs
@@ -1,4 +1,4 @@
-type NumTy = i32;
+type NumTy = usize;
 
 fn main() {
     let input = std::fs::read_to_string("data/input/input15.txt").unwrap();
@@ -10,23 +10,52 @@ fn main() {
 
 fn part1(input: &str) -> String {
     let data = parse_input(input);
-    data.into_iter()
-        .map(|s| {
-            s.as_bytes()
+    data.into_iter().map(hash).sum::<NumTy>().to_string()
+}
+
+fn part2(input: &str) -> String {
+    let data = parse_input(input);
+    let mut table: Vec<Vec<(&str, NumTy)>> = vec![vec![]; 256];
+    for &op in &data {
+        if let Some((label, _)) = op.split_once("-") {
+            let h = hash(label);
+            table[h].retain(|(l, _)| *l != label);
+        } else if let Some((label, lens)) = op.split_once("=") {
+            let h = hash(label);
+            let contents = &mut table[h];
+            match contents.iter_mut().find(|(l, _)| *l == label) {
+                Some((_, old_lens)) => {
+                    *old_lens = lens.parse().unwrap();
+                }
+                None => contents.push((label, lens.parse().unwrap())),
+            }
+        } else {
+            unreachable!("No marker in string: {op}");
+        }
+    }
+
+    table
+        .into_iter()
+        .enumerate()
+        .flat_map(|(box_num, box_contents)| {
+            box_contents
                 .into_iter()
-                .fold(0, |c, n| ((c + (*n as NumTy)) * 17) % 256)
+                .enumerate()
+                .map(move |(slot_num, (_, lens))| (box_num + 1) * (slot_num + 1) * lens)
         })
         .sum::<NumTy>()
         .to_string()
 }
 
-fn part2(input: &str) -> String {
-    "".to_string()
-}
-
-#[allow(unused_imports)]
 fn parse_input(input: &str) -> Vec<&str> {
     input.lines().flat_map(|line| line.split(",")).collect()
+}
+
+fn hash(input: &str) -> NumTy {
+    input
+        .as_bytes()
+        .into_iter()
+        .fold(0, |c, n| ((c + (*n as NumTy)) * 17) % 256)
 }
 
 #[cfg(test)]
@@ -45,8 +74,9 @@ rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7
     #[test]
     fn test_part2() {
         let input = "\
+rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7
 ";
         let result = part2(input);
-        assert_eq!(result, "todo");
+        assert_eq!(result, "145");
     }
 }
