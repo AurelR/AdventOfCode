b20c32441d47d694c9abd820fdbe4c5f764002b1
Aurel Rothärmel <aurel.rothaermel@gmx.de>
2024-01-07 11:04:37 +0100

    Refactor 2023 Day 3

--- rust/aoc2023/src/bin/day-03.rs
+++ rust/aoc2023/src/bin/day-03.rs
@@ -1,4 +1,6 @@
+use itertools::Itertools;
 use std::collections::{BTreeMap, BTreeSet};
+type NumTy = i32;
 
 fn main() {
     let input = std::fs::read_to_string("data/input/input03.txt").unwrap();
@@ -8,143 +10,107 @@ fn main() {
     println!("{}", result2);
 }
 
-type NumTy = i32;
-
 fn part1(input: &str) -> String {
     let mut symbols = BTreeMap::new();
     let mut numbers = BTreeMap::new();
 
-    let mut number = 0;
-    let mut num_mode = false;
-    let mut num_start = 0;
+    parse_input(
+        input,
+        |pos, symbol| {
+            symbols.insert(pos, symbol);
+        },
+        |pos, number| {
+            numbers.insert(pos, number);
+        },
+    );
 
-    for (row, line) in input.lines().enumerate() {
-        let row = row as NumTy;
-        for (col, c) in line.char_indices() {
-            let col = col as NumTy;
-            match c {
-                '.' => {
-                    if num_mode {
-                        num_mode = false;
-                        numbers.insert((row, num_start, col - 1), number);
-                        number = 0;
-                    }
-                }
-                d if '0' <= d && d <= '9' => {
-                    if !num_mode {
-                        num_mode = true;
-                        num_start = col;
-
-                    }
-                    number = number * 10 + d.to_digit(10).expect("Digits expected");
-                }
-                s => {
-                    symbols.insert((row, col), s);
-                    if num_mode {
-                        num_mode = false;
-                        numbers.insert((row, num_start, col - 1), number);
-                        number = 0;
-                    }
-                }
-            }
-        }
-        if num_mode {
-            num_mode = false;
-            numbers.insert((row, num_start, line.as_bytes().len() as NumTy), number);
-            number = 0;
-        }
-    }
-
-    let mut sum = 0;
-    for (&(row, c_begin, c_end) ,&num) in &numbers {
-        let mut has_symbol = false;
-        for r in row-1 ..= row+1 {
-            for c in c_begin-1 ..= c_end+1 {
-                if symbols.contains_key(&(r, c)) {
-                    has_symbol = true;
-                }
-            }
-        }
-        if has_symbol {
-            sum += num;
-        }
-    }
-
-    sum.to_string()
+    numbers
+        .into_iter()
+        .filter(|((row, c_begin, c_end), _number)| {
+            (row - 1..=row + 1)
+                .cartesian_product(c_begin - 1..=c_end + 1)
+                .any(|pos| symbols.contains_key(&pos))
+        })
+        .map(|(_pos, number)| number)
+        .sum::<NumTy>()
+        .to_string()
 }
 
 fn part2(input: &str) -> String {
     let mut symbols = BTreeMap::new();
     let mut numbers = BTreeMap::new();
 
-    let mut number = 0;
-    let mut num_mode = false;
-    let mut num_start = 0;
-
-    for (row, line) in input.lines().enumerate() {
-        let row = row as NumTy;
-        for (col, c) in line.char_indices() {
-            let col = col as NumTy;
-            match c {
-                '.' => {
-                    if num_mode {
-                        num_mode = false;
-                        for c in num_start .. col {
-                            numbers.insert((row, c), number);
-                        }
-                        number = 0;
-                    }
-                }
-                d if '0' <= d && d <= '9' => {
-                    if !num_mode {
-                        num_mode = true;
-                        num_start = col;
-
-                    }
-                    number = number * 10 + d.to_digit(10).expect("Digits expected") as NumTy;
-                }
-                s => {
-                    symbols.insert((row, col), s);
-                    if num_mode {
-                        num_mode = false;
-                        for c in num_start .. col {
-                            numbers.insert((row, c), number);
-                        }
-                        number = 0;
-                    }
-                }
-            }
-        }
-        if num_mode {
-            num_mode = false;
-            for c in num_start ..= line.as_bytes().len() as NumTy {
+    parse_input(
+        input,
+        |pos, symbol| {
+            symbols.insert(pos, symbol);
+        },
+        |(row, col_start, col_end), number| {
+            for c in col_start..=col_end {
                 numbers.insert((row, c), number);
             }
-            number = 0;
-        }
-    }
+        },
+    );
 
-    let mut sum = 0;
-    for (&(row, col) ,&sym) in &symbols {
-        if sym != '*' {
-            continue;
-        }
+    symbols
+        .into_iter()
+        .filter(|&(_pos, symbol)| symbol == '*')
+        .map(|((row, col), _symbol)| {
+            let gears = (row - 1..=row + 1)
+                .cartesian_product(col - 1..=col + 1)
+                .filter_map(|pos| numbers.get(&pos))
+                .collect::<BTreeSet<_>>();
+            if gears.len() == 2 {
+                gears.into_iter().product::<NumTy>()
+            } else {
+                0
+            }
+        })
+        .sum::<NumTy>()
+        .to_string()
+}
 
-        let mut gears = BTreeSet::new();
-        for r in row-1 ..= row+1 {
-            for c in col-1 ..= col+1 {
-                if let Some(&num) = numbers.get(&(r, c)) {
-                    gears.insert(num);
+fn parse_input(
+    input: &str,
+    mut symbol_detected: impl FnMut((NumTy, NumTy), char),
+    mut number_detected: impl FnMut((NumTy, NumTy, NumTy), NumTy),
+) {
+    for (row, line) in input.lines().enumerate() {
+        let row = row as NumTy;
+        let mut number = 0;
+        let mut in_number = false;
+        let mut num_start = 0;
+        for (col, c) in line.char_indices() {
+            let col = col as NumTy;
+            let is_digit = match c {
+                d if '0' <= d && d <= '9' => {
+                    number = number * 10 + d.to_digit(10).expect("Digits expected") as NumTy;
+                    true
                 }
+                '.' => false,
+                s => {
+                    symbol_detected((row, col), s);
+                    false
+                }
+            };
+
+            match (is_digit, in_number) {
+                (true, false) => {
+                    in_number = true;
+                    num_start = col;
+                }
+                (false, true) => {
+                    in_number = false;
+                    number_detected((row, num_start, col - 1), number);
+                    number = 0;
+                }
+                _ => {}
             }
         }
-
-        if gears.len() == 2 {
-            sum += gears.into_iter().product::<NumTy>();
+        if in_number {
+            number_detected((row, num_start, line.as_bytes().len() as NumTy), number);
         }
     }
-
-    sum.to_string()
 }
 
 #[cfg(test)]
