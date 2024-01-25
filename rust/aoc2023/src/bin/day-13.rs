8c792219e61f3ed4a86ec3c925fa0a1580e5e879
Aurel Rothärmel <aurel.rothaermel@gmx.de>
2024-01-25 01:14:16 +0100

    Refactor 2023 Day 13

--- rust/aoc2023/src/bin/day-13.rs
+++ rust/aoc2023/src/bin/day-13.rs
@@ -53,62 +53,64 @@ struct Grid {
 
 impl Grid {
     fn score1(&self) -> NumTy {
-        self.horizontal_reflection().map_or_else(
-            || self.transpose().horizontal_reflection().unwrap_or(0),
-            |s| s * 100,
-        )
+        self.horizontal_reflection()
+            .map_or_else(|| self.vertical_reflection().unwrap_or(0), |s| s * 100)
     }
 
     fn score2(&self) -> NumTy {
-        self.horizontal_miss_1().map_or_else(
-            || self.transpose().horizontal_miss_1().unwrap_or(0),
-            |s| s * 100,
-        )
+        self.horizontal_miss_1()
+            .map_or_else(|| self.vertical_miss_1().unwrap_or(0), |s| s * 100)
     }
 
     fn horizontal_reflection(&self) -> Option<NumTy> {
-        for i in 1..self.y_size {
+        (1..self.y_size).find(|&i| {
             let len = i.min(self.y_size / 2).min(self.y_size - i);
             let a = &self.contents[i - len..i];
             let b = &self.contents[i..i + len];
-            if a.iter().rev().eq(b.iter()) {
-                return Some(i);
-            }
-        }
-        None
+            a.iter().rev().eq(b.iter())
+        })
+    }
+
+    fn vertical_reflection(&self) -> Option<NumTy> {
+        (1..self.x_size).find(|&i| {
+            let len = i.min(self.x_size / 2).min(self.x_size - i);
+            self.contents.iter().all(|row| {
+                let a = &row[i - len..i];
+                let b = &row[i..i + len];
+                a.iter().rev().eq(b.iter())
+            })
+        })
     }
 
     fn horizontal_miss_1(&self) -> Option<NumTy> {
-        for i in 1..self.y_size {
+        (1..self.y_size).find(|&i| {
             let len = i.min(self.y_size / 2).min(self.y_size - i);
             let a = &self.contents[i - len..i];
             let b = &self.contents[i..i + len];
-            if a.iter()
+            a.iter()
                 .rev()
-                .zip(b)
-                .map(|(u, v)| u.iter().zip(v.iter()).filter(|(z, w)| z != w).count())
-                .sum::<usize>()
+                .flatten()
+                .zip(b.iter().flatten())
+                .filter(|(z, w)| z != w)
+                .count()
                 == 1
-            {
-                return Some(i);
-            }
-        }
-        None
+        })
     }
 
-    fn transpose(&self) -> Grid {
-        Grid {
-            x_size: self.y_size,
-            y_size: self.x_size,
-            contents: (0..self.x_size)
-                .map(|i| {
-                    self.contents
-                        .iter()
-                        .map(|inner| inner[i].clone())
-                        .collect::<Vec<_>>()
+    fn vertical_miss_1(&self) -> Option<NumTy> {
+        (1..self.x_size).find(|&i| {
+            let len = i.min(self.x_size / 2).min(self.x_size - i);
+            self.contents
+                .iter()
+                .flat_map(|row| {
+                    let a = &row[i - len..i];
+                    let b = &row[i..i + len];
+                    a.iter().rev().zip(b.iter())
                 })
-                .collect::<Vec<_>>(),
-        }
+                .filter(|(z, w)| z != w)
+                .count()
+                == 1
+        })
     }
 }
 
