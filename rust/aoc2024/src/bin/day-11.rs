1c4d0c5005d895b446346ef743f41dbc0eec1a09
Aurel Rothärmel <aurel.rothaermel@gmx.de>
2024-12-11 23:23:02 +0100

    2024: Solve Day 11 Part 2

--- rust/aoc2024/src/bin/day-11.rs
+++ rust/aoc2024/src/bin/day-11.rs
@@ -1,4 +1,8 @@
+use std::collections::BTreeMap;
+
 type NumTy = i64;
+
+type Cache = BTreeMap<(NumTy, NumTy), NumTy>;
 
 fn main() {
     let input = std::fs::read_to_string("data/input/input11.txt").unwrap();
@@ -17,7 +21,13 @@ fn part1(input: &str) -> String {
 }
 
 fn part2(input: &str) -> String {
-    "".to_string()
+    let data = parse(input);
+    let mut result = 0;
+    let mut cache = Cache::new();
+    for n in data {
+        result += find_expansion(n, 75, &mut cache);
+    }
+    result.to_string()
 }
 
 fn parse(input: &str) -> Vec<NumTy> {
@@ -56,6 +66,22 @@ fn count_digits(mut num: NumTy) -> NumTy {
     count
 }
 
+fn find_expansion(num: NumTy, its: NumTy, cache: &mut Cache) -> NumTy {
+    if its == 0 {
+        return 1;
+    }
+    if let Some(r) = cache.get(&(num, its)) {
+        return *r;
+    }
+
+    let mut result = 0;
+    for d in blink(vec![num]) {
+        result += find_expansion(d, its - 1, cache)
+    }
+    cache.insert((num, its), result);
+    result
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -71,11 +97,11 @@ mod tests {
 
     #[test]
     fn test_part2() {
-        let input = "0 1 10 99 999
+        let input = "125 17
 ";
 
         let result = part2(input);
-        assert_eq!("00000", result);
+        assert_eq!("65601038650482", result);
     }
 
     #[test]
