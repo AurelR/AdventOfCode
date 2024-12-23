5877dbdefa99da6255454e7b0db9d891ecbeca5d
Aurel Rothärmel <aurel.rothaermel@gmx.de>
2024-12-23 16:51:29 +0100

    2024: Solve Day 23 Part 2

--- rust/aoc2024/src/bin/day-23.rs
+++ rust/aoc2024/src/bin/day-23.rs
@@ -1,6 +1,7 @@
+use itertools::Itertools;
 use std::collections::{BTreeMap, BTreeSet};
 
-type Node<'a> = &'a str; //[u8; 2];
+type Node<'a> = &'a str;
 type Nodes<'a> = BTreeSet<Node<'a>>;
 type Edges<'a> = BTreeSet<(Node<'a>, Node<'a>)>;
 type AdjList<'a> = BTreeMap<Node<'a>, Nodes<'a>>;
@@ -40,8 +41,42 @@ fn part1(input: &str) -> String {
 }
 
 fn part2(input: &str) -> String {
-    let (_nodes, _edges, _adj_list) = parse(input);
-    "".to_string()
+    let (_nodes, edges, adj_list) = parse(input);
+
+    let mut candiates = edges
+        .into_iter()
+        .map(|(n1, n2)| {
+            let mut c = BTreeSet::new();
+            c.insert(n1);
+            c.insert(n2);
+            c
+        })
+        .collect::<BTreeSet<_>>();
+
+    let mut cliques = BTreeSet::new();
+    while candiates.len() != 0 {
+        let mut new_candiates = BTreeSet::new();
+        'outer: for set in candiates {
+            let mut it = set.iter();
+            let first = *it.next().unwrap();
+            for &n in &adj_list[first] {
+                if it.clone().all(|&m| adj_list[m].contains(n)) {
+                    let mut new_candidate = set.clone();
+                    new_candidate.insert(n);
+                    new_candiates.insert(new_candidate);
+                    continue 'outer;
+                }
+            }
+            cliques.insert(set);
+        }
+        candiates = new_candiates;
+    }
+
+    let max = cliques
+        .into_iter()
+        .max_by(|a, b| a.len().cmp(&b.len()))
+        .unwrap();
+    max.into_iter().intersperse(",").collect()
 }
 
 fn parse(input: &str) -> (Nodes, Edges, AdjList) {
@@ -52,8 +87,6 @@ fn parse(input: &str) -> (Nodes, Edges, AdjList) {
         .map(|l| {
             let n1: Node = &l[0..2];
             let n2: Node = &l[3..5];
-            //let n1: Node = l.as_bytes()[0..2].try_into().unwrap();
-            //let n2: Node = l.as_bytes()[3..5].try_into().unwrap();
             nodes.insert(n1);
             nodes.insert(n2);
             adj_list.entry(n1).or_default().insert(n2);
@@ -113,7 +146,6 @@ td-yn
     }
 
     #[test]
-    #[ignore = "todo"]
     fn test_part2() {
         let input = "kh-tc
 qp-kh
